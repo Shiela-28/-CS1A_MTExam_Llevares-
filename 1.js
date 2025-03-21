d33da38bf2aee46c6a73ae78b7bb902992294335
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-21 21:25:21 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+//1: User Information Collection:
+
+let fullName = prompt("Enter your full name:");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
