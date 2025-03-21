6560f212d29669c07fb09293f373b06db6cb5ff7
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-21 21:25:50 +0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,9 @@
+//2: User Information Collection:
+
+let subjectTitle = prompt("Enter the subject title:");
+let classSchedule = prompt("Enter the class schedule (Time, Day):");
+let classroom = prompt("Enter the classroom:");
+let instructor = prompt("Enter the instructor's name:");
+
+console.log{subjectTitle} is currently enrolled. 
+The instructor for the subject is {instructor}. Scheduled at room ${classroom}.);
